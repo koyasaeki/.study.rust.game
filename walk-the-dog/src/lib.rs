fef4ac017b1eef3b21d79efea7b23b29902df0af
KoyaSaeki <koyasaeki@gmail.com>
2023-07-24 21:09:39 +0900

    update: Idle を表示する

--- walk-the-dog/src/lib.rs
+++ walk-the-dog/src/lib.rs
@@ -1,105 +1,68 @@
-use rand::prelude::*;
+use std::rc::Rc;
+use std::sync::Mutex;
+
+use futures::channel::oneshot::channel;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::spawn_local;
+use web_sys::HtmlCanvasElement;
+use web_sys::{CanvasRenderingContext2d, HtmlImageElement};
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
-    let window = web_sys::window().unwrap();
-    let document = window.document().unwrap();
-    let canvas = document
-        .get_element_by_id("canvas")
-        .unwrap()
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .unwrap();
-    let context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
+    let context = get_canvas_context();
 
-    wasm_bindgen_futures::spawn_local(async move {
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<()>();
-        let image = web_sys::HtmlImageElement::new().unwrap();
+    spawn_local(async move {
+        let (sender, receiver) = channel::<Result<(), JsValue>>();
 
-        let callback = Closure::once(|| {
-            success_tx.send(());
+        // 複数のスレッド間で sender を共有するために Mutex 型にする。
+        // 複数のスレッドで sender を使いたいので Rc 型にする。
+        let success_sender = Rc::new(Mutex::new(Some(sender)));
+        // sender をエラー発生時にも使いたいのでカウンタを増やす。
+        let error_sender = Rc::clone(&success_sender);
+        let image = HtmlImageElement::new().unwrap();
+
+        let success_callback = Closure::once(move || {
+            if let Some(success_sender) = success_sender.lock().ok().and_then(|mut opt| opt.take())
+            {
+                success_sender.send(Ok(()));
+            }
         });
-        image.set_onload(Some(callback.as_ref().unchecked_ref()));
-        callback.forget();
+        let error_callback = Closure::once(move |err| {
+            if let Some(error_sender) = error_sender.lock().ok().and_then(|mut opt| opt.take()) {
+                error_sender.send(Err(err));
+            }
+        });
 
         image.set_src("Idle (1).png");
-        success_rx.await;
-        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-        sierpinski(
-            &context,
-            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-            (200, 50, 100),
-            5,
-        );
+
+        // 画像の設定に成功したときの処理
+        image.set_onload(Some(success_callback.as_ref().unchecked_ref()));
+        // 画像の設定に失敗したときの処理
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
+
+        receiver.await;
+        context.draw_image_with_html_image_element(&image, 300.0, 300.0);
     });
 
     Ok(())
 }
 
-fn draw_triangle(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-) {
-    let [top, left, right] = points;
-    let color_str = format!("rgb({},{},{})", color.0, color.1, color.2);
-    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
+fn get_canvas_context() -> CanvasRenderingContext2d {
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<HtmlCanvasElement>()
+        .unwrap();
 
-    context.move_to(top.0, top.1);
-    context.begin_path();
-    context.line_to(left.0, left.1);
-    context.line_to(right.0, right.1);
-    context.line_to(top.0, top.1);
-    context.close_path();
-    context.stroke();
-    context.fill();
-}
-
-fn mid_point(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
-    ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
-}
-
-fn sierpinski(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-    depth: u8,
-) {
-    let [top, left, right] = points;
-    draw_triangle(context, [top, left, right], color);
-
-    let mut rng = thread_rng();
-    let new_color = (
-        rng.gen_range(0..255),
-        rng.gen_range(0..255),
-        rng.gen_range(0..255),
-    );
-
-    let depth = depth - 1;
-    if depth > 0 {
-        let left_middle = mid_point(top, left);
-        let right_middle = mid_point(top, right);
-        let bottom_middle = mid_point(left, right);
-        sierpinski(context, [top, left_middle, right_middle], new_color, depth);
-        sierpinski(
-            context,
-            [left_middle, left, bottom_middle],
-            new_color,
-            depth,
-        );
-        sierpinski(
-            context,
-            [right_middle, bottom_middle, right],
-            new_color,
-            depth,
-        );
-    }
+    canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<CanvasRenderingContext2d>()
+        .unwrap()
 }
